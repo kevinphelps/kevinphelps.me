9cf501a0c57deeb4eb4b0c25aaefab8e2f57a742
Kevin <KevinPhelps11@gmail.com>
2014-10-21 19:06:26 -0500

    click thumbnails to see fullres image

--- js/site.js
+++ js/site.js
@@ -1,3 +1,4 @@
+/********************************** About Me **********************************/
 function showAboutMe(buttonToRemove)
 {
   $("#about-me").hide(); // hide so that I can fade it after removing classes
@@ -7,3 +8,19 @@ function showAboutMe(buttonToRemove)
   $("html, body").animate({ scrollTop: $("footer").offset().top }, 1000);
   $(buttonToRemove).remove();
 }
+
+/********************************* Thumbnails *********************************/
+$(".thumbnail[data-fullres-src]").click(function()
+{
+  // create modal for fullres image
+  var $modal = $("<div/>", { "class": "modal fade" }).insertAfter($(this));
+  var $modalDialog = $("<div/>", { "class": "modal-dialog" }).appendTo($modal);
+  var $modalContent = $("<div/>", { "class": "modal-content" }).appendTo($modalDialog);
+  $("<img/>", { "class": "col-md-12 thumbnail", "src": $(this).attr("data-fullres-src") }).appendTo($modalContent);
+
+  // set event to remove modal
+  $modal.on('hidden.bs.modal', function (e) { $modal.remove(); });
+
+  // show the modal
+  $modal.modal();
+});
