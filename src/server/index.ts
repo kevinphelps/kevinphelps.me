efb0f377f60d178617ee31b88dbabed1481d2287
kevinphelps <KevinPhelps11@gmail.com>
2017-04-08 18:53:06 -0500

    refactor: clean up server render code

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,40 +4,25 @@ import 'zone.js/dist/zone-node';
 import * as bodyParser from 'body-parser';
 import * as express from 'express';
 import * as expressLogging from 'express-logging';
-import * as fs from 'fs';
 import * as http from 'http';
 import * as logops from 'logops';
 
+import { enableProdMode } from '@angular/core';
+
 import { environment } from '../environments/environment';
-import { serverRender } from './utilities/server-render';
+import { serverRenderHandler } from './handlers/server-render.handler';
+import { indexFallbackHandler, staticFilesHandler } from './handlers/static-files.handler';
 
-export const indexHtml = fs.readFileSync('./dist/client/index.html').toString();
+enableProdMode();
 
-(async function() {
-  const app = express();
-  const server = http.createServer(app);
-  const staticFiles = express.static('./dist/client', { redirect: false });
+const app = express();
+const server = http.createServer(app);
 
-  app.use(expressLogging(logops));
-  app.use(bodyParser.json());
+app.use(expressLogging(logops));
+app.use(bodyParser.json());
 
-  app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    if (req.method === 'GET' && req.accepts('html')) {
-      serverRender(req, res).subscribe(() => { });
-    } else {
-      next();
-    }
-  });
+app.use(serverRenderHandler);
+app.use(staticFilesHandler);
+app.use(indexFallbackHandler);
 
-  app.use(staticFiles);
-
-  app.use((req: express.Request, res: express.Response, next: express.NextFunction) => {
-    if (req.method === 'GET' && req.accepts('html')) {
-      res.contentType('text/html').send(indexHtml);
-    } else {
-      next();
-    }
-  });
-
-  server.listen(environment.serverPort, () => { console.log(`listening on port ${environment.serverPort}`); });
-})();
+server.listen(environment.serverPort, () => { console.log(`listening on port ${environment.serverPort}`); });
