97283ffb7634275daa733ebcc04389d410175763
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-22 21:49:23 -0500

    refactor: create common environment file

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,7 @@
-export const environment = {
-  production: true,
-  serverPort: typeof window === 'undefined' ? process.env.PORT || 5000 : undefined,
-  api: typeof window === 'undefined' ? 'http://localhost:1114/api' : '/api',
+import { commonEnvironment } from './environment.common';
+
+const prodEnvironment = {
+  production: true
 };
+
+export const environment = { ...commonEnvironment, ...prodEnvironment };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,7 @@
-// The file contents for the current environment will overwrite these during build.
-// The build system defaults to the dev environment which uses `environment.ts`, but if you do
-// `ng build --env=prod` then `environment.prod.ts` will be used instead.
-// The list of which env maps to which file can be found in `.angular-cli.json`.
+import { commonEnvironment } from './environment.common';
 
-export const environment = {
+const devEnvironment = {
   production: false,
-  serverPort: typeof window === 'undefined' ? process.env.PORT || 5000 : undefined,
-  api: typeof window === 'undefined' ? 'http://localhost:1114/api' : '/api',
 };
+
+export const environment = { ...commonEnvironment, ...devEnvironment };
