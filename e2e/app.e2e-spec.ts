b7ea5c684fb7f5e4923d3b7aaf6122fdb993b577
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-22 21:32:48 -0500

    test: fix build error in e2e code

--- e2e/app.e2e-spec.ts
+++ e2e/app.e2e-spec.ts
@@ -9,6 +9,8 @@ describe('home page', () => {
 
   it('should display message saying app works', () => {
     page.navigateTo();
-    expect(page.getParagraphText()).toEqual('app works!');
+
+    page.getParagraphText()
+      .then(text => { expect(text).toEqual('app works!'); });
   });
 });
