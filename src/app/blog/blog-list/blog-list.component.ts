82049c2113be13c655334f5d8ea78b52399376a5
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-25 18:41:38 -0500

    fix: load blog list (remove testing code)

--- src/app/blog/blog-list/blog-list.component.ts
+++ src/app/blog/blog-list/blog-list.component.ts
@@ -11,8 +11,7 @@ export class BlogListComponent implements OnInit {
   readonly blogList: Observable<BlogEntry[]>;
 
   constructor(private blog: BlogService) {
-    this.blogList = this.blog.getBlogEntry('2017-01-17', 'using-angular-dependency-injection-in-node')
-      .map(entry => [entry]);
+    this.blogList = this.blog.getBlogList();
   }
 
   ngOnInit() {
