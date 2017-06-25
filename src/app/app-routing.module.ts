3b62e80ff06112d6f6f36dd913b6019438f6d71e
Kevin Phelps <KevinPhelps11@gmail.com>
2017-06-25 17:24:01 -0500

    refactor: create move layout to separate components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,17 +5,22 @@ import { BlogEntryComponent } from './blog/blog-entry/blog-entry.component';
 import { BlogListComponent } from './blog/blog-list/blog-list.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ResumeComponent } from './pages/resume/resume.component';
+import { DefaultLayoutComponent } from './shared/components/default-layout/default-layout.component';
 import { ProfileHeaderComponent } from './shared/components/profile-header/profile-header.component';
 
 export const routes: Routes = [
   {
-    path: '', component: ProfileHeaderComponent, children: [
-      { path: '', component: BlogListComponent },
-      { path: 'resume', component: ResumeComponent }
+    path: '', component: DefaultLayoutComponent, children: [
+      {
+        path: '', component: ProfileHeaderComponent, children: [
+          { path: '', component: BlogListComponent },
+          { path: 'resume', component: ResumeComponent }
+        ]
+      },
+      { path: 'blog/:date/:urlSlug', component: BlogEntryComponent },
+      { path: '**', component: NotFoundComponent }
     ]
-  },
-  { path: 'blog/:date/:urlSlug', component: BlogEntryComponent },
-  { path: '**', component: NotFoundComponent }
+  }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,9 @@ import { BlogEntryComponent } from './blog/blog-entry/blog-entry.component';
 import { BlogListComponent } from './blog/blog-list/blog-list.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 import { ResumeComponent } from './pages/resume/resume.component';
+import { DefaultFooterComponent } from './shared/components/default-footer/default-footer.component';
+import { DefaultHeaderComponent } from './shared/components/default-header/default-header.component';
+import { DefaultLayoutComponent } from './shared/components/default-layout/default-layout.component';
 import { ProfileHeaderComponent } from './shared/components/profile-header/profile-header.component';
 
 @NgModule({
@@ -23,6 +26,9 @@ import { ProfileHeaderComponent } from './shared/components/profile-header/profi
     AppComponent,
     BlogEntryComponent,
     BlogListComponent,
+    DefaultFooterComponent,
+    DefaultHeaderComponent,
+    DefaultLayoutComponent,
     NotFoundComponent,
     ProfileHeaderComponent,
     ResumeComponent
